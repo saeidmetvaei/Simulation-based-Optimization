b28c7e13d17a1c628c5bd648762d17132a561805
Saeid Metvaei <59529804+saeidmetvaei@users.noreply.github.com>
2022-08-06 12:42:06 +0430

    Add files via upload

---
+++ Cost.vb
@@ -0,0 +1,11 @@
+﻿Public Class Cost
+
+    Public Function CalCost(ByVal MyTime As Double, ByVal MyUnitCost As Double) As Double
+        Dim MyCost As Double
+
+        MyCost = MyTime * MyUnitCost
+
+        Return MyCost
+    End Function
+
+End Class

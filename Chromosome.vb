ab57683b6f11af9eff5314efb35d47b556e6de02
Saeid Metvaei <59529804+saeidmetvaei@users.noreply.github.com>
2022-08-06 12:40:02 +0430

    Add files via upload

---
+++ Chromosome.vb
@@ -0,0 +1,14 @@
+﻿Public Class Chromosome
+
+    Public ID As Integer
+
+    Public Sequencey_genomes As New Dictionary(Of Integer, Double)
+    Public Resource_genomes As New Dictionary(Of Integer, Double)
+    Public total_genomes As New Dictionary(Of Integer, Double)
+
+    Public fitness As Integer
+    Public MadeBy As String
+
+    Public total_cost As Double
+
+End Class

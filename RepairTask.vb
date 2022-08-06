7fd3466a8a6aee6fe1dff55a8d715127d16d7b2d
Saeid Metvaei <59529804+saeidmetvaei@users.noreply.github.com>
2022-08-06 12:41:17 +0430

    Add files via upload

---
+++ RepairTask.vb
@@ -0,0 +1,45 @@
+﻿Imports Simphony.Simulation
+Public Class RepairTask
+
+    Inherits Entity
+    Public Property TaskName As String
+    Public Property TaskDescription As String
+    Public Property Status As String
+
+    Public Property Priority As Integer
+    Public Property EsTime As Double
+    Public Property ResourceN As Integer
+    Public Property RequiredSkill As String
+    Public Property UsedSkill As String
+    Public Property TaskTech As Technician
+    Public Property TaskFile As WaitingFile
+    Public Property TechnicianID As String
+    Public Property TechnicianName As String
+    Public Property MustSkilled As Boolean
+    Public Property ReportDate As Date
+    Public Property AssignedTime As Date
+    Public Property StartTime As Date
+    Public Property EndTime As Date
+    Public Property CostTech As Double = 0
+    Public Property CostTravel As Double = 0
+    Public Property CostDelay As Double = 0
+    Public Property TotalCost As Double = 0
+    Public Property RealTotalCost As Double = 0
+    Public Property Type As String = ""
+
+    'For GA Class
+    Public Property TaskID As Integer
+    Public Property SequenceNumber As Integer
+
+    'For loss calculation
+    Public Property Capital As Boolean
+    Public Property PatientsNoPerHour As Integer
+    Public Property PatientCost As Double
+
+    Public Property LossPerHour As Double
+
+    'For DES/CBR
+    Public Property TaskGroupTech As New List(Of Technician)
+    Public Property GetRes As Boolean
+
+End Class
---
+++ Technecian.vb
@@ -0,0 +1,29 @@
+﻿Imports Simphony.Simulation
+
+Public Class Technician
+
+    Inherits Resource
+    Public Property TechSkill As String
+    Public Property TechName As String
+    Public Property TechID As Integer
+    Public Property UnitCost As Double
+    Public Property PreviousTask As RepairTask
+    Public Property InitialTechLocation As String
+    Public Property TotalTaskCost As Double
+    'for Greedy algorithm
+    Public Property CurrentTask As RepairTask
+    'for Dijkstar
+    Public Property Path As New List(Of RepairTask)
+    Public Sub New()
+        Me.Servers = 0
+    End Sub
+
+    Public Sub New(profession As String)
+        Me.TechSkill = profession
+        Me.Servers = 0
+        'Me.TechName = Name
+
+    End Sub
+
+
+End Class
